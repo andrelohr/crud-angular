d7d0043e6ef5671f2a452b8a6da36deb5474818a
Andre Lohr <andrefelipelohr@gmail.com>
2023-06-08 13:28:11 -0300

    Atualizacao de repositorio 4

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,4 +1,9 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-course-form',
@@ -7,10 +12,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseFormComponent implements OnInit{
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private snackBar: MatSnackBar,
+    private location: Location) {
+    this.form = this.formBuilder.group({
+      name: [null],
+      category: [null]
+    });
+   }
 
   ngOnInit(): void {
 
   }
 
+  onSubmit() {
+    this.service.save(this.form.value)
+      .subscribe(result => this.onSucess(), error => this.onError());
+  }
+
+  onCancel() {
+    this.location.back();
+  }
+
+  private onSucess() {
+    this.snackBar.open('Curso salvo com sucesso.', '', { duration: 5000 });
+    this.onCancel();
+  }
+
+  private onError() {
+    this.snackBar.open('Erro ao salvar curso.', '', { duration: 5000 });
+  }
 }
